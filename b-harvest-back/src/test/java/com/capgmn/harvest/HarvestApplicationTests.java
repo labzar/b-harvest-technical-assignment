e4d0532cd83840fca8b245111e45d509a267355c
labzar <labzar.othmane@gmail.com>
2022-05-18 21:59:33 +0200

    feat: add back socle

---
+++ b-harvest-back/src/test/java/com/capgmn/harvest/HarvestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.capgmn.harvest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HarvestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
